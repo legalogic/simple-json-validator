37bb41cf445d3622db756a1e69ac3303a55b27a3
arik <arikkrol@gmail.com>
2020-07-05 23:04:04 +0300

    cont

--- validator.js
+++ validator.js
@@ -15,6 +15,59 @@ const validatePrimitive = (schema, value, path) => {
   }
 }
 
+/**
+ * Supported primitives: string / number / boolean / object
+ * @param {*[]} schema 
+ * @param {*} value 
+ * @param {string[]} path 
+ */
+const validateArray = (schema, value, path) => {
+  // TODO: complete
+}
+
+/**
+ * Supported primitives: string / number / boolean / object
+ * @param {Object} schema 
+ * @param {*} value 
+ * @param {string[]} path 
+ */
+const validateObject = (schema, value, path) => {
+  if (!schema) { // schema == null
+    return
+  }
+
+  // at this point schema is an object so value must be an object to pass
+  if (!value || (typeof value).toLowerCase() != 'object' || Array.isArray(value)) {
+    throw new ValidationError(path)
+  }
+
+  // both schema and value are objects
+  for (const key of schema) {
+    const val = schema[key]
+    // TODO: complete
+  }
+
+}
+
+/**
+ * @param {Object} schema 
+ * @param {*} value 
+ * @param {string[]} path 
+ */
+const validateComplex = (schema, value, path) => {
+  if (Array.isArray(schema)) {
+    validateArray(schema, value, path)
+  } else {
+    validateObject(schema, value, path)
+  }
+}
+
+/**
+ * Recursively validate value using schema. On error throw exception with the path to the failed field.
+ * @param {*} schema 
+ * @param {*} value 
+ * @param {*} path 
+ */
 const validateRec = (schema, value, path) => {
   const schemaType = (typeof schema).toLowerCase()
   switch (schemaType) {
@@ -22,7 +75,10 @@ const validateRec = (schema, value, path) => {
       validatePrimitive(schema, value, path)
       break
     case 'object':
-      // TODO: complete
+      validateComplex(schema, value, path)
+      break
+    case 'undefined':
+      // do nothing
       break
     default:
       throw new Error(`schema may only be of type string or object but was neither. schema is:${schema}`)
